afb08bce7c96ec8f611f6d5ad565060ce0ab33e0
TrudyCyns <gcyn51a@gmail.com>
2022-05-26 19:47:39 +0300

    Finished For Loop

--- for_loop/ch44.py
+++ for_loop/ch44.py
@@ -1,8 +1,12 @@
-# Ask how many people the user wants to invite to a party. If  10, ask for the names and after each name display “[name] has been invited”. If they enter a number which is 10 or higher, display the message “Too many people”.
+# Ask how many people the user wants to invite to a party. If then enter a number below 10, ask for the names and after each name display “[name] has been invited”. If they enter a number which is 10 or higher, display the message “Too many people”.
 
-# Ask user to enter name
-fname = input("Please enter your name: ")
+# Ask user to a number
+guests = int(input("How many people do you want to invite to a party?: "))
 
-# Print name three times.
-for i in range(1,4):
-    print(fname)
+# Check if greater than or less than 10.
+if guests < 10:
+    for i in range(0,guests):
+        invite = input("Enter the name or guest: ")
+        print(f"{invite} has been invited.")
+else:
+    print("Too many people!")  
